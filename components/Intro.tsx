55d1de404de1144912427bceb1c5817d34e1b541
Franco Avolio <francoavolio@hotmail.com>
2024-05-29 17:39:56 -0300

    added cv download and contact me button

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -2,10 +2,13 @@
 import React from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsGit, BsGithub, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
 
 export default function Intro() {
   return (
-    <section>
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -25,11 +28,36 @@ export default function Intro() {
           </motion.div>
         </div>
       </div>
-      <p className="font-medium italic text-xl leading-tight mb-10 mt-6 px-4 sm:text-2xl">
+      <motion.p
+        className="font-medium italic text-xl leading-tight mb-10 mt-6 px-6 sm:text-2xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 2, y: 0 }}
+      >
         <strong>Hello! I'm Franco Avolio </strong>, a passionate{" "}
         <strong>Full Stack Developer </strong> with a strong background in
         building robust and scalable web applications.
-      </p>
+      </motion.p>
+      <motion.div className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium" 
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 2, y: 0 }}
+        transition={{ delay: 0.3 }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition-all"
+        >
+          Contact me <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+        <a className="group cursor-pointer bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition" href="/CV.pdf" download={true}>
+          Download CV <HiDownload className="opacity-60 group-hover:translate-x-1"/>
+        </a>
+        <a className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full">
+          <BsLinkedin />
+        </a>
+        <a className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full">
+          <BsGithub />
+        </a>
+      </motion.div>
     </section>
   );
 }
